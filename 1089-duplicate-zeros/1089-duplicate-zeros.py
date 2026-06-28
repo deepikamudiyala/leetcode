01397579168a3dee79381079806cea28b62804f4
deepikamudiyala <mudiyaladeepika1509@gmail.com>
2026-06-28 20:47:14 +0530

    Time: 819 ms (5.04%), Space: 19.1 MB (99.71%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -0,0 +1,17 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i=0
+        while i<len(arr):
+            if arr[i]==0:
+                j=len(arr)-1
+                while j>i+1: 
+                    arr[j]=arr[j-1]
+                    j-=1
+                if i+1 <len(arr):
+                    arr[i+1]=0
+                i+=2
+            else:
+                i+=1
