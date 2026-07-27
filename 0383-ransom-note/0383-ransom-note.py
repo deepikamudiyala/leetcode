d862fa36f42b5ff8513234dcfe72d3cd5d01e34e
deepikamudiyala <mudiyaladeepika1509@gmail.com>
2026-07-27 21:30:27 +0530

    Time: 29 ms (15.44%), Space: 19.5 MB (71.72%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count=0
+        a={}
+        for i in magazine:
+            if i not in a:
+                a[i]=1
+            else:
+                a[i]+=1
+        for i in ransomNote:
+            if i in a and a[i]>0:
+                a[i]-=1
+            else:
+                return False
+        return True
