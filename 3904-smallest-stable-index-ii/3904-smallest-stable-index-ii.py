ff35a7989fd8612712306e1d812b14bcde263c1c
deepikamudiyala <mudiyaladeepika1509@gmail.com>
2026-09-05 12:09:52 +0530

    Time: 135 ms (89.55%), Space: 33 MB (40%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -4,11 +4,13 @@ class Solution:
         premin=[0]*len(nums)
         maxi=float('-inf')
         for i in range(len(nums)):
-            maxi=max(maxi,nums[i])
+            if nums[i]>maxi:
+                maxi=nums[i]
             premax[i]=maxi
         mini=float('inf')
         for i in range(len(nums)-1,-1,-1):
-            mini=min(mini,nums[i])
+            if nums[i]<mini:
+                mini=nums[i]
             premin[i]=mini
         for i in range(len(nums)):
             if premax[i]-premin[i]<=k:
